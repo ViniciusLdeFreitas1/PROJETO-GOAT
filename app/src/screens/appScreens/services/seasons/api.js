e95cb45f5a2dc5aa06d7a96e30fc057c300879a2
Gustavo Oliveira Antunes da Silva <119068163+GustavoAntunes07@users.noreply.github.com>
2024-09-26 13:29:04 +0000

    troquei o flatlist das temporadas da página home para as transferencias(mudar nome para temporadas) e adicionei um texto na home

--- app/src/screens/appScreens/services/seasons/api.js
+++ app/src/screens/appScreens/services/seasons/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://api-nba-v1.p.rapidapi.com",
   headers: {
-    "x-rapidapi-key": "api-key", 
+    "x-rapidapi-key": "7fa880eb43msh5d32f8e9f689be4p1459efjsn6eb1f0a5d54f", 
     "x-rapidapi-host": "api-basketball.p.rapidapi.com",
   },
 });
