391c98ac612d77bf7e6886ec2ec0c2f203481565
GustavoAntunes07 <Viajantebrasileiro3@gmail.com>
2024-10-07 14:56:33 -0300

    Sistema de favoritar times pronto

--- app/src/screens/appScreens/Perfil/Perfil.js
+++ app/src/screens/appScreens/Perfil/Perfil.js
@@ -7,23 +7,68 @@ import {
   View,
   Image,
   ScrollView,
+  FlatList,
 } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as ImagePicker from "expo-image-picker";
-import { FontAwesome } from '@expo/vector-icons';
-import { useUser } from '../../../components/UserContext';
-import goatlogo from '../../../../../assets/goatlogo.png';
+import { FontAwesome } from "@expo/vector-icons";
+import { useUser } from "../../../components/UserContext";
+import goatlogo from "../../../../../assets/goatlogo.png";
+import axios from "axios";
 
 export default function Perfil({ navigation }) {
   const { userData, updateUserData } = useUser();
-  const [profileImage, setProfileImage] = useState(userData.profileImage || '');
+  const [profileImage, setProfileImage] = useState(userData.profileImage || "");
+  const [favoriteTeams, setFavoriteTeams] = useState([]);
 
   useEffect(() => {
     (async () => {
-      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      const { status } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
       if (status !== "granted") {
-        Alert.alert("Permissão necessária", "Precisamos de acesso à sua galeria.");
+        Alert.alert(
+          "Permissão necessária",
+          "Precisamos de acesso à sua galeria."
+        );
       }
     })();
+
+    const loadFavorites = async () => {
+      const storedFavorites = await AsyncStorage.getItem("favoriteTeams");
+      if (storedFavorites) {
+        const favoriteIds = JSON.parse(storedFavorites);
+        console.log("Favorite IDs recuperados:", favoriteIds); // Verifique os favoritos recuperados
+
+        // Buscando dados da API
+        const options = {
+          method: "GET",
+          url: "https://api-basketball.p.rapidapi.com/teams",
+          params: {
+            league: "12",
+            season: "2023-2024",
+          },
+          headers: {
+            "x-rapidapi-key":
+              "7fa880eb43msh5d32f8e9f689be4p1459efjsn6eb1f0a5d54f",
+            "x-rapidapi-host": "api-basketball.p.rapidapi.com",
+          },
+        };
+
+        try {
+          const response = await axios.request(options);
+          const allTeams = response.data.response;
+
+          const favoriteTeamsList = allTeams.filter((team) =>
+            favoriteIds.includes(team.id)
+          );
+          setFavoriteTeams(favoriteTeamsList);
+        } catch (error) {
+          console.error("Erro ao buscar times:", error);
+        }
+      }
+    };
+
+    loadFavorites();
   }, []);
 
   const pickImage = async () => {
@@ -39,10 +84,10 @@ export default function Perfil({ navigation }) {
         const selectedImageUri = result.assets[0].uri;
         setProfileImage(selectedImageUri);
         await updateUserData({ profileImage: selectedImageUri });
-        Alert.alert('Sucesso', 'Imagem de perfil atualizada com sucesso.');
+        Alert.alert("Sucesso", "Imagem de perfil atualizada com sucesso.");
       }
     } catch (error) {
-      Alert.alert('Erro', 'Falha ao selecionar a imagem.');
+      Alert.alert("Erro", "Falha ao selecionar a imagem.");
     }
   };
 
@@ -52,29 +97,36 @@ export default function Perfil({ navigation }) {
         <View style={styles.profile}>
           <TouchableOpacity onPress={pickImage} style={styles.imageContainer}>
             {profileImage ? (
-              <Image source={{ uri: profileImage }} style={styles.profileImage} />
+              <Image
+                source={{ uri: profileImage }}
+                style={styles.profileImage}
+              />
             ) : (
               <Image source={goatlogo} style={styles.placeholder} />
             )}
           </TouchableOpacity>
           <Text style={styles.profileName}>{userData.username}</Text>
         </View>
-        <View style={styles.actions}>
-          <TouchableOpacity style={styles.actionButton} onPress={() => navigation.navigate('Likes')}>
-            <View style={styles.card}>
-              <FontAwesome name="heart" size={24} color="red" />
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.actionButton} onPress={() => navigation.navigate('Notifications')}>
-            <View style={styles.card}>
-              <FontAwesome name="bell" size={24} color="yellow" />
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.actionButton} onPress={() => navigation.navigate('Settings')}>
-            <View style={styles.card}>
-              <FontAwesome name="cog" size={24} color="white" />
-            </View>
-          </TouchableOpacity>
+        <View style={styles.FavoriteContainer}>
+          <Text style={styles.header}>Favorite Teams</Text>
+          {favoriteTeams.length > 0 ? (
+            <FlatList
+              data={favoriteTeams}
+              keyExtractor={(item) => item.id.toString()}
+              renderItem={({ item }) => (
+                <View style={styles.teamCard}>
+                  <Image
+                    source={{ uri: item.logo }} // Agora estamos buscando a logo da API
+                    style={styles.logo}
+                    resizeMode="contain"
+                  />
+                  <Text style={styles.teamName}>{item.name}</Text>
+                </View>
+              )}
+            />
+          ) : (
+            <Text style={styles.noFavorites}>No favorite teams yet.</Text>
+          )}
         </View>
       </ScrollView>
     </View>
@@ -84,28 +136,28 @@ export default function Perfil({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#54514F',
-    justifyContent: 'center',
+    backgroundColor: "#333",
+    justifyContent: "center",
   },
   scrollContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     padding: 20,
     paddingTop: 180,
   },
   profile: {
-    alignItems: 'center',
+    alignItems: "center",
     marginBottom: 40,
   },
   imageContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     marginBottom: 10,
   },
   profileImage: {
     width: 150,
     height: 150,
     borderRadius: 75,
-    resizeMode: 'cover',
+    resizeMode: "cover",
   },
   placeholder: {
     width: 150,
@@ -113,26 +165,50 @@ const styles = StyleSheet.create({
     borderRadius: 75,
   },
   profileName: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   actions: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    width: '100%',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
   },
   actionButton: {
     padding: 10,
-    alignItems: 'center',
+    alignItems: "center",
     marginHorizontal: 10,
   },
   card: {
-    backgroundColor: '#7D7875',
+    backgroundColor: "#7D7875",
     padding: 15,
     borderRadius: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     marginLeft: 10,
   },
+  teamCard: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  logo: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  teamName: {
+    fontSize: 18,
+    color: "#fff",
+  },
+  noFavorites: {
+    fontSize: 16,
+    color: "#888",
+  },
+  FavoriteContainer: {
+    backgroundColor: "#444",
+    borderRadius: 5,
+    width: 275,
+    height: 175
+  }
 });
